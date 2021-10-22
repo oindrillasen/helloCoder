3f388407e1ae02e1229df18508d1bff5de2fb06a
oindrillasen <oindrilla.sen@wgu.edu>
2021-10-22 02:00:35 -0600

    First Commit

---
+++ HelloCoders.py
@@ -0,0 +1,16 @@
+#Import Flask Library
+from flask import Flask
+
+#Initialize your app from Flask
+
+app = Flask(__name__)
+
+#Define your route that could be called
+
+@app.route('/')
+def hellocoders():
+    return "<B>Enjoy Big Data and Dev Conference"
+
+#Run the app
+app.run()
+
